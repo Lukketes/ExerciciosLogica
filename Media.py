e32f4a6bafadad2a445a467d11bde4753ca8d06e
unknown <lucasfreittasoficial@gmail.com>
2025-03-29 23:33:02 -0300

    Alteração no exercicio da media

--- Media.py
+++ Media.py
@@ -4,6 +4,10 @@ Valor2 = int(input(f'Digite o valor 2: '))
 Valor3 = int(input(f'Digite o valor 3: '))
 soma = int
 
+#Condição
+if Valor1 < 0 or Valor2 < 0 or Valor3 < 0:
+    print("Erro: número negativo")
+
 # Conta
 soma = Valor1 + Valor2 + Valor3
 media = soma / 3
